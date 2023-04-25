e9687f668de3c6a92e0f23416cc32b022ad729ac
Essam Alsaud <essamx123@gmail.com>
2023-04-25 13:37:51 +0300

    Done

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function filterByTeams(){
+function filterByAnyTeam() {
+    let Userinput = document.getElementById("search");
+    let searchWord = Userinput.value.toLowerCase();
+    let tables = document.querySelectorAll(".Schedule");
+    for (table of tables) {
+      let rows = document.querySelectorAll("tbody tr");
+      doFilter(rows, 1, searchWord);
+    }
+  }
+
+  function filterByTopTeams() {
+    let dropDownChoice = document.getElementsByClassName("Teams")[0];
+    let searchWord = dropDownChoice.value.toLowerCase();
+    let tables = document.querySelectorAll(".Schedule");
+    for (table of tables) {
+      let rows = document.querySelectorAll("tbody tr");
+      doFilter(rows, 1, searchWord);
+    }
+  }
+
+  function doFilter(rows, column, searchWord) {
     
-}
+    for (let i = 0; i < rows.length; i++) {
+      let td = rows[i].getElementsByTagName("td")[column];
+      let txtValue = td.innerText
+      if (txtValue.toLowerCase().indexOf(searchWord) > -1) {
+        rows[i].style.display = "";
+      } else {
+        rows[i].style.display = "none";
+      }
+    }
+  }
